e98f76cf1cf2f01793d6d6ae4e9d13d7d30d91a6
datsjustinc <jc9957@nyu.edu>
2023-05-01 19:18:17 -0400

    Player Cam and lighting adjustments

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Player
+{
+    /// <summary>
+    /// This class allows camera to move with player.
+    /// </summary>
+    public class MoveCamera : MonoBehaviour
+    {
+        // camera object on player to track
+        [SerializeField] private Transform cameraPosition;
+        
+        // getters and setters
+        public Transform CameraPosition { get => cameraPosition; set => cameraPosition = value; }
+
+        /// <summary>
+        /// This function is called every frame.
+        /// </summary>
+        private void Update()
+        {
+            // move camera to player camera position
+            transform.position = cameraPosition.position;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace Player
+{
+    /// <summary>
+    /// This class implements player movement physics.
+    /// </summary>
+    public class PlayerCamera : MonoBehaviour
+    {
+        // mouse sensitivity
+        [SerializeField] private float sensX;
+        [SerializeField] private float sensY;
+
+        // player orientation stores facing direction
+        public Transform orientation;
+        
+        // rotation of camera
+        [SerializeField] private float xRotation;
+        [SerializeField] private float yRotation;
+        
+        // getters and setters
+        public float SensX { get => sensX; set => sensX = value; }
+        public float SensY { get => sensY; set => sensY = value; }
+        public Transform Orientation { get => orientation; set => orientation = value; }
+        public float XRotation { get => xRotation; set => xRotation = value; }
+        public float YRotation { get => yRotation; set => yRotation = value; }
+
+        /// <summary>
+        /// This function is called at start of game.
+        /// </summary>
+        private void Start()
+        {
+            // makes sure cursor is in center of screen and invisible
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        /// <summary>
+        /// This function is called every frame.
+        /// </summary>
+        private void Update()
+        {
+            // get mouse inputs
+            var mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            var mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+            
+            yRotation += mouseX;
+            xRotation -= mouseY;
+
+            // clamp x axis rotation
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            
+            // rotate camera and orientation
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+    }
+}
+
